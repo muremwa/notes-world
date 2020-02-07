31ece5ae8aa93ec755fe69d312c82e3aa909c4cf
Muremwa <danmburu254@gmail.com>
2020-02-07 16:54:05 +0300

    sign up for updated

--- account/forms.py
+++ account/forms.py
@@ -9,12 +9,17 @@ from .models import Profile
 import re
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
+    first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={"class": "form-control"}))
+    last_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={"class": "form-control"}))
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'password1', 'password2',)
+        widgets = {
+            'username': forms.TextInput(attrs={"class": "form-control"}),
+            'password1': forms.PasswordInput(attrs={"class": "form-control"}),
+            'password2': forms.PasswordInput(attrs={"class": "form-control"}),
+        }
 
     def clean_username(self):
         data = self.cleaned_data['username']
