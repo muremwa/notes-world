1a76d48da2f384e248a0d3082201269fd35f71e8
Muremwa <danmburu254@gmail.com>
2020-03-25 15:14:52 +0300

    account forms natural

--- account/forms.py
+++ account/forms.py
@@ -32,12 +32,24 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ("pen_name", "occupation", "gender", "image")
+        widgets = {
+            'pen_name': forms.TextInput(attrs={"class": "form-control"}),
+            'occupation': forms.TextInput(attrs={"class": "form-control"}),
+            'gender': forms.Select(attrs={"class": "form-control"}),
+            'image': forms.ClearableFileInput(attrs={"class": "form-control"})
+        }
 
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email',)
+        widgets = {
+            'username': forms.TextInput(attrs={"class": "form-control"}),
+            'first_name': forms.TextInput(attrs={"class": "form-control"}),
+            'last_name': forms.TextInput(attrs={"class": "form-control"}),
+            'email': forms.EmailInput(attrs={"class": "form-control"})
+        }
 
     def clean_username(self):
         data = self.cleaned_data['username']
