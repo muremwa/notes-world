157f29bc3b930a50da0a4315bbcd00ac3f5d465b
Muremwa <danmburu254@gmail.com>
2021-06-17 18:37:20 +0300

    small update

--- notes/admin.py
+++ notes/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Note, Comment, Reply, Tag
 
 
-admin.site.register(Note)
 admin.site.register(Tag)
 admin.site.register(Comment)
 admin.site.register(Reply)
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    exclude = ('last_modified', 'last_modifier')
+    list_display = ('title', 'user', 'privacy', 'collaborative')
