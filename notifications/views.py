130cfbf94bc7d595f374cf406875a10e96c27570
Daniel Muremwa Mburu <daniel.muremwa@cellulant.io>
2022-12-18 16:10:52 +0300

    auth users fetch notifications

--- notifications/views.py
+++ notifications/views.py
@@ -1,17 +1,20 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, reverse
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 from .models import Notification
 
 
 # new notifications
+@login_required
 def new_notification(request):
     response = {'new': request.user.notification_set.filter(seen=False).count()}
     return JsonResponse(response)
 
 
 # mark a notification as opened
+@login_required
 def make_opened(request, notification_id):
     result = dict()
     if request.method == 'POST':
@@ -28,6 +31,7 @@ def make_opened(request, notification_id):
 
 
 # delete notification
+@login_required
 def delete_notification(request, notification_id):
     if request.method == "POST":
         notification = get_object_or_404(Notification, pk=notification_id)
@@ -37,6 +41,7 @@ def delete_notification(request, notification_id):
             return JsonResponse({'success': True, "message": "Notification deleted"})
 
 
+@login_required
 def bulk_delete_notifications(request):
     notifications_deleted = 0
 
